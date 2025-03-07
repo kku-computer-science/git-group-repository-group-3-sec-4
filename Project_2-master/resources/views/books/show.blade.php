817385621a5cd3315f2f968bd65cb4b811923df1
jeerawan-singkin <jeerawan240846.@gmail.com>
2025-03-08 00:05:58 +0700

    เปลี่ยน show.blade , create.blade แล้ว

--- Project_2-master/resources/views/books/show.blade.php
+++ Project_2-master/resources/views/books/show.blade.php
@@ -4,32 +4,29 @@
 <div class="container">
     <div class="card col-md-8" style="padding: 16px;">
         <div class="card-body">
-            <h4 class="card-title">Book Detail</h4>
-            <p class="card-description">ข้อมูลรายละเอียดหนังสือ</p>
+            <h4 class="card-title">{{ trans('books.Book Detail') }}</h4>
+            <p class="card-description">{{ trans('books.Book details information') }}</p>
             <div class="row">
-                <p class="card-text col-sm-3"><b>ชื่อหนังสือ</b></p>
+                <p class="card-text col-sm-3"><b>{{ trans('books.Book name') }}</b></p>
                 <p class="card-text col-sm-9">{{ $paper->ac_name }}</p>
             </div>
             <div class="row">
-                <p class="card-text col-sm-3"><b>ปี</b></p>
-                <p class="card-text col-sm-9">{{  date('Y', strtotime($paper->ac_year))+543 }}</p>
+                <p class="card-text col-sm-3"><b>{{ trans('books.Year') }}</b></p>
+                <p class="card-text col-sm-9">{{ date('Y', strtotime($paper->ac_year)) + 543 }}</p>
             </div>
             <div class="row">
-                <p class="card-text col-sm-3"><b>แหล่งเผยแพร่</b></p>
+                <p class="card-text col-sm-3"><b>{{ trans('books.Publication source') }}</b></p>
                 <p class="card-text col-sm-9">{{ $paper->ac_sourcetitle }}</p>
             </div>
             <div class="row">
-                <p class="card-text col-sm-3"><b>หน้า</b></p>
+                <p class="card-text col-sm-3"><b>{{ trans('books.Page') }}</b></p>
                 <p class="card-text col-sm-9">{{ $paper->ac_page }}</p>
             </div>
 
             <div class="pull-right mt-5">
-                <a class="btn btn-primary btn-sm" href="{{ route('books.index') }}"> Back</a>
+                <a class="btn btn-primary btn-sm" href="{{ route('books.index') }}">{{ trans('books.Back') }}</a>
             </div>
         </div>
-
     </div>
-
-
 </div>
 @endsection
